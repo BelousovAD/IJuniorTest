7523c75f5418120b3e9d03bdd9729a096799d2ea
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-06-07 21:15:56 +0800

    Исправлено место инициализации вращения

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,16 +4,11 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private Mover _mover;
     [SerializeField] private MaterialSetter _materialSetter;
-    [SerializeField] private float _maxAbsStartRotation = 180f;
+    
     [SerializeField] private float _startSpeed = 1f;
 
     public void Initialize()
     {
-        Vector3 rotation = new(
-            0f,
-            Random.Range(-_maxAbsStartRotation, _maxAbsStartRotation),
-            0f);
-        transform.Rotate(rotation);
         _mover.Speed = _startSpeed;
         _materialSetter.SetRandomMaterial();
     }
--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,12 +5,14 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private Enemy _prefab;
     [SerializeField] private Transform _parent;
+    [SerializeField] private float _maxAbsStartRotation = 180f;
     [SerializeField] private List<Transform> _spawnPoints = new();
 
     public void SpawnAtRandomPoint()
     {
         Transform spawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Count)];
-        Enemy enemy = Instantiate(_prefab, spawnPoint.position, Quaternion.identity, _parent);
+        Vector3 rotation = new(0f, Random.Range(-_maxAbsStartRotation, _maxAbsStartRotation), 0f);
+        Enemy enemy = Instantiate(_prefab, spawnPoint.position, Quaternion.Euler(rotation), _parent);
         enemy.Initialize();
     }
 }
