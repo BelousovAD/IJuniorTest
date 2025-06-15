fbfc16d135c678338a667f35095b4f44bc9ede3f
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-06-15 22:54:11 +0800

    Добавлена проверка слоёв для PlayerIsOnGroundChecker

--- Assets/Scripts/PlayerIsOnGroundChecker.cs
+++ Assets/Scripts/PlayerIsOnGroundChecker.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class PlayerIsOnGroundChecker : MonoBehaviour
 {
     [SerializeField] private float _checkDistance = 0.1f;
-    [SerializeField] private Collider2D _playerCollider;
+    [SerializeField] private LayerMask _layersToRaycast;
 
     private RaycastHit2D _hitInfo;
     private bool _isOnGround;
@@ -13,9 +13,9 @@ public class PlayerIsOnGroundChecker : MonoBehaviour
 
     private void Update()
     {
-        _hitInfo = Physics2D.Raycast(transform.position, Vector2.down, _checkDistance);
+        _hitInfo = Physics2D.Raycast(transform.position, Vector2.down, _checkDistance, _layersToRaycast);
 
-        if ((_hitInfo.collider != null && _hitInfo.collider != _playerCollider) ^ _isOnGround)
+        if (_hitInfo.collider != null ^ _isOnGround)
         {
             _isOnGround = _hitInfo.collider != null;
             IsOnGroundStatusChanged?.Invoke(_isOnGround);
