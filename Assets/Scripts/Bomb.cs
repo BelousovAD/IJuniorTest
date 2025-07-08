e758c7c480a95031b62ff76e4a169430c6095c3c
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-07-08 14:28:31 +0800

    Создан компонент взрыва бомбы

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Explosion))]
 [RequireComponent(typeof(RandomValueCoroutineTimer))]
+[RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(SmoothVanisher))]
 public class Bomb : PooledObject
 {
+    private Explosion _explosion;
     private RandomValueCoroutineTimer _timer;
+    private Rigidbody _rigidbody;
     private SmoothVanisher _vanisher;
 
     private void Awake()
     {
+        _explosion = GetComponent<Explosion>();
         _timer = GetComponent<RandomValueCoroutineTimer>();
+        _rigidbody = GetComponent<Rigidbody>();
         _vanisher = GetComponent<SmoothVanisher>();
     }
 
@@ -18,12 +24,14 @@ public class Bomb : PooledObject
         _timer.Started += HandleTimerStart;
         _timer.TimeIsUp += Release;
         _timer.StartTimer();
+        _rigidbody.WakeUp();
     }
 
     private void OnDisable()
     {
         _timer.Started -= HandleTimerStart;
         _timer.TimeIsUp -= Release;
+        _rigidbody.Sleep();
     }
 
     private void HandleTimerStart() =>
@@ -32,6 +40,7 @@ public class Bomb : PooledObject
     public override void Release()
     {
         _vanisher.StopVanish();
+        _explosion.Explode(transform.position);
         base.Release();
     }
 }
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] private float _force = 1f;
+    [SerializeField] private float _radius = 1f;
+
+    public void Explode(Vector3 point)
+    {
+        List<Rigidbody> affectedRigidbodies = GetRigidbodiesFromExplosionSphere(point, _radius);
+        
+        foreach (Rigidbody rigidbody in affectedRigidbodies)
+        {
+            rigidbody.AddExplosionForce(_force, point, _radius);
+        }
+    }
+
+    private List<Rigidbody> GetRigidbodiesFromExplosionSphere(Vector3 point, float radius)
+    {
+        Collider[] colliders = Physics.OverlapSphere(point, radius);
+        List<Rigidbody> rigidbodies = new();
+        Rigidbody attachedRigidbody;
+
+        foreach (Collider collider in colliders)
+        {
+            attachedRigidbody = collider.attachedRigidbody;
+
+            if (attachedRigidbody is not null)
+            {
+                rigidbodies.Add(attachedRigidbody);
+            }
+        }
+
+        return rigidbodies;
+    }
+}
