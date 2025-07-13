1a2532fd229b1ff640c97a690ea42765a1689cb4
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-07-13 17:11:44 +0800

    Создан PooledComponent

---
+++ Assets/Scripts/Common/Pool/PooledComponent.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Common.Pool
+{
+    public class PooledComponent : MonoBehaviour
+    {
+        private IObjectPool<PooledComponent> _pool;
+
+        public virtual void Initialize(IObjectPool<PooledComponent> pool) =>
+            _pool = pool;
+
+        public virtual void Release() =>
+            _pool.Release(this);
+    }
+}
