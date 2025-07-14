b659e0e79e27fb0e4db550515baf59a2dde8e265
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-07-14 14:09:53 +0800

    Добавлена возможность убивать противников

--- Assets/Scripts/Character/Enemy/Enemy.cs
+++ Assets/Scripts/Character/Enemy/Enemy.cs
@@ -1,7 +1,17 @@
 using Common.Pool;
+using UnityEngine;
+using Weapon.Bullet;
 
 namespace Character.Enemy
 {
     public class Enemy : PooledComponent
-    { }
+    {
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.TryGetComponent<Bullet>(out _))
+            {
+                Release();
+            }
+        }
+    }
 }
