6acb7f33f4e635489d7f4bc7dd8875a6530f65fe
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-07-17 10:04:57 +0800

    Создан скрипт стрелка для врага

---
+++ Assets/Scripts/Character/Enemy/Shooter.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Character.Enemy
+{
+    public class Shooter : Character.Shooter
+    {
+        [SerializeField] private PlayerDetector _detector;
+        
+        private Coroutine _shooting;
+
+        private void OnEnable() =>
+            _detector.ValueChanged += Shoot;
+
+        private void OnDisable() =>
+            _detector.ValueChanged -= Shoot;
+
+        private void Shoot()
+        {
+            if (_detector.Value)
+            {
+                _shooting = StartCoroutine(Shooting());
+            }
+            else
+            {
+                StopCoroutine(_shooting);
+            }
+        }
+    }
+}
