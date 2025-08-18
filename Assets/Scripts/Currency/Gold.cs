d210b45741cc7684bc1737b9d2589799c93666b9
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-08-18 14:02:06 +0800

    Добавлена валюта золота

---
+++ Assets/Scripts/Currency/Gold.cs
@@ -0,0 +1,56 @@
+namespace Currency
+{
+    using System;
+    using UnityEngine;
+
+    public class Gold
+    {
+        private const int MinValue = 0;
+        private const int MaxValue = int.MaxValue;
+
+        private int _value;
+
+        public event Action ValueChanged;
+
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+            
+            protected set
+            {
+                _value = Mathf.Clamp(value, MinValue, MaxValue);
+                ValueChanged?.Invoke();
+            }
+        }
+
+        public void Earn(int amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Can not be negative");
+            }
+
+            Value += amount;
+        }
+
+        public bool TrySpend(int amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Can not be negative");
+            }
+
+            if (Value < amount)
+            {
+                return false;
+            }
+            
+            Value -= amount;
+            
+            return true;
+        }
+    }
+}
