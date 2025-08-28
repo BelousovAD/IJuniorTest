e95fba8f968b92c26c62516bf5985fb945759e3f
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-08-28 21:54:28 +0800

    Созданы скрипты IPickable и Picker

---
+++ Assets/Scripts/Pickable/IPickable.cs
@@ -0,0 +1,5 @@
+namespace Pickable
+{
+    public interface IPickable
+    { }
+}
---
+++ Assets/Scripts/Pickable/Picker.cs
@@ -0,0 +1,18 @@
+namespace Pickable
+{
+    using System;
+    using UnityEngine;
+
+    public class Picker : MonoBehaviour
+    {
+        public event Action<IPickable> Picking;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.TryGetComponent(out IPickable pickable))
+            {
+                Picking?.Invoke(pickable);
+            }
+        }
+    }
+}
