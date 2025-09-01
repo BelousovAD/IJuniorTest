cda4de11052369e822afe0724c3c60e613446abe
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-09-01 21:16:27 +0800

    Добавлено вращение монетам

--- Assets/Scripts/Item/Gold.cs
+++ Assets/Scripts/Item/Gold.cs
@@ -3,11 +3,14 @@ namespace Item
     using Common.Spawn;
     using Pickable;
     using UnityEngine;
+    using Random = UnityEngine.Random;
 
     [RequireComponent(typeof(Collider))]
     [RequireComponent(typeof(Rigidbody))]
     public class Gold : PooledComponent, IPickable
     {
+        [SerializeField, Min(0)] private float _maxAngularVelocity = 7f;
+        
         private Collider _collider;
         private Rigidbody _rigidbody;
 
@@ -15,7 +18,11 @@ namespace Item
         {
             _collider = GetComponent<Collider>();
             _rigidbody = GetComponent<Rigidbody>();
+            _rigidbody.maxAngularVelocity = _maxAngularVelocity;
         }
+
+        private void OnEnable() =>
+            _rigidbody.AddRelativeTorque(Random.onUnitSphere * _maxAngularVelocity, ForceMode.Impulse);
 
         public void PickUp()
         {
@@ -27,7 +34,6 @@ namespace Item
         {
             _collider.enabled = true;
             _rigidbody.isKinematic = false;
-            _rigidbody.Sleep();
         }
     }
 }
