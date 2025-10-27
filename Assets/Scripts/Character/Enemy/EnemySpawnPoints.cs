4b490b81155e77af32e93bfc3d7b022cfb9277d3
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-10-27 21:44:11 +0800

    Добавлены точки спавна врагов

---
+++ Assets/Scripts/Character/Enemy/EnemySpawnPoints.cs
@@ -0,0 +1,19 @@
+namespace Character.Enemy
+{
+    using System.Collections.Generic;
+    using Common.Spawn;
+    using UnityEngine;
+
+    public class EnemySpawnPoints : MonoBehaviour
+    {
+        [SerializeField] private Spawner _spawner;
+        [SerializeField] private List<Transform> _points = new();
+
+        public Vector3 GetRandomPoint()
+        {
+            Transform point = _points[Random.Range(0, _points.Count)];
+
+            return point.position;
+        }
+    }
+}
---
+++ Assets/Scripts/Common/Spawn/Spawner.cs
@@ -0,0 +1,51 @@
+namespace Common.Spawn
+{
+    using System;
+    using UnityEngine;
+    using UnityEngine.Pool;
+
+    public class Spawner : MonoBehaviour
+    {
+        [SerializeField] private Transform _parent;
+        [SerializeField] private PooledComponent _prefab;
+        [SerializeField, Min(1)] private int _poolSize = 1;
+
+        private IObjectPool<PooledComponent> _pool;
+
+        public event Action<PooledComponent> ComponentReleased;
+
+        protected virtual void Awake() =>
+            _pool = new ObjectPool<PooledComponent>(
+                createFunc: CreatePooledComponent,
+                actionOnRelease: ReleasePooledComponent,
+                actionOnDestroy: DestroyPooledComponent,
+                defaultCapacity: _poolSize);
+
+        protected PooledComponent SpawnAt(Vector3 position)
+        {
+            PooledComponent pooledComponent = _pool.Get();
+            pooledComponent.transform.position = position;
+            pooledComponent.ReleaseRequested += _pool.Release;
+            pooledComponent.gameObject.SetActive(true);
+
+            return pooledComponent;
+        }
+
+        private void ReleasePooledComponent(PooledComponent pooledComponent)
+        {
+            pooledComponent.gameObject.SetActive(false);
+            pooledComponent.ReleaseRequested -= _pool.Release;
+            ComponentReleased?.Invoke(pooledComponent);
+        }
+
+        private PooledComponent CreatePooledComponent()
+        {
+            PooledComponent pooledComponent = Instantiate(_prefab, _parent);
+
+            return pooledComponent;
+        }
+
+        private void DestroyPooledComponent(PooledComponent pooledComponent) =>
+            Destroy(pooledComponent.gameObject);
+    }
+}
